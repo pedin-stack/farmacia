c7428777be88f8879e3decc8b88981ae96110126
Pedro Dourado Rocha <pdourado2006@gmail.com>
2025-12-27 02:53:15 -0300

    configuração : conexão com o backend remoto

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const BASE_URL = 'https://farmacia-back-dm6j.onrender.com';
 
 const api = axios.create({
     baseURL: BASE_URL,
